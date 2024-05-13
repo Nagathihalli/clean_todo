07fdec4b6d4a13611870a78f688718794f2045bb
bharath <bharath@elementure.tech>
2024-05-13 16:24:26 +0530

    clearing the codes

---
+++ lib/widgets/categories_selection.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CategorySelection extends StatelessWidget {
+  const CategorySelection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
